250b6b0d1d5ae939eb129edc1e14006e50e82153
Pat Adams <patick_aust@hotmail.com>
2025-06-17 09:41:05 +0200

    added test

---
+++ test.py
@@ -0,0 +1,19 @@
+print('Hello World')
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('This is meee')
+
+
+import dbpy, stpy, ippy
+
+print('Life should beeee')
+
+import ecpy
+
+print('Fun for everyone')
+
+
+
